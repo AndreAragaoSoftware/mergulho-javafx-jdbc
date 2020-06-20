e42b1217e4c8945c8d9cd912e3141df2ed043301
André Aragão <64162795+AndreAragaoSoftware@users.noreply.github.com>
2020-06-20 13:14:59 -0300

    Inicializando ação como parâmetro

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -34,43 +35,23 @@ public class MainViewController implements Initializable {
 	@FXML
 	public void onMenuItemFuncaoAction() {
 		// loadView("/gui/FuncaoList.fxml");
-		loadView2("/gui/FuncaoList.fxml");
+		loadView("/gui/FuncaoList.fxml", (FuncaoListController controller) -> {
+			controller.setFuncaoService(new FuncaoService());
+			controller.updateTableView();
+		});
+		;
 	}
 
 	@FXML
 	public void onMenuItemCadastrarClinicoAction() {
-		loadView2("/gui/CadastrarClinico.fxml");
+		loadView("/gui/CadastrarClinico.fxml", x -> {});
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 	}
 
-//	private synchronized void loadView(String absoluteName) {
-//		try {
-//			// Instanciando  FXMLLoader para poder abrir nova tela
-//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-//			VBox newVBox = loader.load();
-//
-//			Scene mainScene = Main.getMainScene();
-//			// o getRoot pega o primeiro elemento da View(no caso o ScrollPane)
-//			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-//
-//			//Primeiro filho do VBox na janela principal(mainMenu)
-//			Node mainMenu = mainVBox.getChildren().get(0);
-//			//limpando todos os filhos do main VBox
-//			mainVBox.getChildren().clear();
-//
-//			//add os mainMenu e o newVBox
-//			mainVBox.getChildren().add(mainMenu);
-//			mainVBox.getChildren().addAll(newVBox.getChildren());
-//		}
-//		catch (IOException e) {
-//			Alerts.showAlert("IOException", "Error loadding view", e.getMessage(), AlertType.ERROR);
-//		}
-//	}
-
-	private synchronized void loadView2(String absoluteName) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
 			// Instanciando FXMLLoader para poder abrir nova tela
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
@@ -89,12 +70,11 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
 
-			// associando o funcaoList ao loader
-			FuncaoListController controller = loader.getController();
-			controller.setFuncaoService(new FuncaoService());
-			controller.updateTableView();
+			T controller = loader.getController();
+			initializingAction.accept(controller);
 		} catch (IOException e) {
 			Alerts.showAlert("IOException", "Error loadding view", e.getMessage(), AlertType.ERROR);
 		}
 	}
+
 }
