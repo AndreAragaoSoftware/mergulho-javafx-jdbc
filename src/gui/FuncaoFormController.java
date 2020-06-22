5515e0426904e205ae9cbe8147c17f045b10259d
André Aragão <64162795+AndreAragaoSoftware@users.noreply.github.com>
2020-06-22 12:45:41 -0300

    FuncaoFormController

---
+++ src/gui/FuncaoFormController.java
@@ -0,0 +1,49 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class FuncaoFormController implements Initializable {
+
+	@FXML
+	private TextField txtId;
+	@FXML
+	private TextField txtName;
+	@FXML
+	private Label labelErrorName;
+	@FXML
+	private Button btSalvar;
+	@FXML
+	private Button btCancelar;
+	
+	@FXML
+	public void onBtSalvarAction() {
+		System.out.println("onBtSalvarAction");
+	}
+	
+	@FXML
+	public void onBtCancelarAction() {
+		System.out.println("onBtCancelarAction");
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+	
+	// retrinções 
+		private void initializeNodes() {
+			// o txtId só aceita numero Inteiro
+			Constraints.setTextFieldInteger(txtId);
+			// o txtName só pode ter no maximo 60 caracters
+			Constraints.setTextFieldMaxLength(txtName, 60);
+		}
+
+}
