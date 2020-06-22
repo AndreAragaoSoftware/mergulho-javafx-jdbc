dfcbc26939f0d25db8e7398479387c03aa5f0613
André Aragão <64162795+AndreAragaoSoftware@users.noreply.github.com>
2020-06-22 12:30:19 -0300

    Design do FuncaoForm (diálogo)

--- src/gui/FuncaoListController.java
+++ src/gui/FuncaoListController.java
@@ -1,73 +1,105 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Funcao;
 import model.services.FuncaoService;
+import javafx.event.ActionEvent;
 
 public class FuncaoListController implements Initializable {
-	
+
 	private FuncaoService service;
 
 	@FXML
 	private TableView<Funcao> tableViewFuncao;
-	
+
 	@FXML
 	private TableColumn<Funcao, Integer> tableColumnId;
-	
+
 	@FXML
 	private TableColumn<Funcao, Double> tableColumnNome;
-	
+
 	@FXML
 	private Button btNova;
-	
+
 	private ObservableList<Funcao> obsList;
-	
+
 	@FXML
-	public void onBtNovaAction() {
-		System.out.println("onBtNovaAction");
+	public void onBtNovaAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/FuncaoForm.fxml", parentStage);
 	}
-	
+
 	public void setFuncaoService(FuncaoService service) {
 		this.service = service;
 	}
-	
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
 	}
 
 	private void initializeNodes() {
-		//padrão para iniciar o comportamento das colunas
+		// padrão para iniciar o comportamento das colunas
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnNome.setCellValueFactory(new PropertyValueFactory<>("name"));
 
-		//macete para que a tableView acompanhe a janela
+		// macete para que a tableView acompanhe a janela
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewFuncao.prefHeightProperty().bind(stage.heightProperty());
 
 	}
-	
+
 	public void updateTableView() {
 		if (service == null) {
 			throw new IllegalStateException("Serviço foi nulo");
 		}
-		
+
 		List<Funcao> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewFuncao.setItems(obsList);
 	}
-	
+
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			// Instanciando FXMLLoader para poder abrir nova tela
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			// carregando o pane
+			Pane pane = loader.load();
+
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department data");
+			dialogStage.setScene(new Scene(pane));
+			// não pode ser rederizada
+			dialogStage.setResizable(false);
+			// chamando o palco
+			dialogStage.initOwner(parentStage);
+			// só pode fazer outra coisa se for fechada
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Erro ao carregar a tela", e.getMessage(), AlertType.ERROR);
+		}
+	}
 }
